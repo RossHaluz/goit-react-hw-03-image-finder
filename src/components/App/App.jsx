dd5a39b7f114ffcffa7f798e0fed7106c442099a
Rostyslav <rossgaluzinskiy@gmail.com>
2023-01-23 19:53:51 +0200

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,13 +3,26 @@ import toast, { Toaster } from 'react-hot-toast';
 import Searchbar from 'components/Searchbar';
 import { Container } from './App.styled';
 import ImageGallery from 'components/ImageGallery';
+import FetchPhotosGallery from 'components/api/api';
+// import ImageLoader from 'components/Loader/Loader';
 
 class App extends Component {
   state = {
     name: '',
     items: [],
     page: 1,
+    error: null,
+    loading: false,
   };
+
+  async componentDidUpdate(prevProp, prevState) {
+    if (prevState.name !== this.state.name) {
+      console.log('Змінили імя');
+      const images = await FetchPhotosGallery(this.state.name, this.state.page);
+      console.log(images);
+      this.setState({ items: images.hits });
+    }
+  }
 
   getNameSerch = name => {
     if (name === '') {
@@ -23,7 +36,7 @@ class App extends Component {
     return (
       <Container>
         <Searchbar onSubmit={this.getNameSerch} />
-        <ImageGallery nameSerach={this.state.name} />
+        <ImageGallery items={this.state.items} />
         <Toaster position="top-right" />
       </Container>
     );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,65 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ImageGalleryList } from './ImageGallery.styled';
 import ImageGalleryItem from 'components/ImageGalleryItem';
-import FetchPhotosGallery from 'components/api/api';
-import ImageLoader from 'components/Loader/Loader';
-import LoadMore from 'components/LoadMoreBtn';
 
-class ImageGallery extends Component {
-  state = {
-    items: [],
-    page: 1,
-    error: null,
-    loading: false,
-  };
-
-  async componentDidUpdate(prevProp, prevState) {
-    const prevNameSearch = prevProp.nameSerach;
-    const nextNameSearch = this.props.nameSerach;
-    if (
-      prevNameSearch !== nextNameSearch ||
-      prevState.page !== this.state.page
-    ) {
-      this.setState({
-        loading: true,
-        error: null,
-        items: [],
-      });
-      try {
-        const images = await FetchPhotosGallery(
-          nextNameSearch,
-          this.state.page
-        );
-        if (images.hits.length === 0) {
-          throw new Error();
-        }
-        this.setState({ items: [...images.hits] });
-      } catch (error) {
-        this.setState({ error });
-      } finally {
-        this.setState({ loading: false });
-      }
-    }
-  }
-
-  onChangePageNumber = () => {
-    this.setState({ page: 1 });
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-
-  render() {
-    const { items, loading, error } = this.state;
-    return (
-      <>
-        <ImageGalleryList>
-          {error && <p>Упс.. щось пішло не так :( Оновіть сторінку.</p>}
-          {loading && <ImageLoader />}
-          <ImageGalleryItem items={items} />
-        </ImageGalleryList>
-        {items.length > 11 && <LoadMore addPage={this.onChangePageNumber} />}
-      </>
-    );
-  }
-}
+const ImageGallery = ({ items }) => {
+  return (
+    <ImageGalleryList>
+      <ImageGalleryItem items={items} />
+    </ImageGalleryList>
+  );
+};
 
 export default ImageGallery;
